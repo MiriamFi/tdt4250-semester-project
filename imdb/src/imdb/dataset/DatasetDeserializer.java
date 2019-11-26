9a5b2853387b72a2c96b93407cf5204ee0e8fc11
Timmy Chan <fancygamer123@gmail.com>
2019-11-26 15:28:40 +0100

    Add decent deserialize for person

--- imdb/src/imdb/dataset/DatasetDeserializer.java
+++ imdb/src/imdb/dataset/DatasetDeserializer.java
@@ -12,26 +12,24 @@ import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
 
 import imdb.Imdb;
 import imdb.ImdbFactory;
-import imdb.ImdbPackage;
+import imdb.Person;
 import imdb.Title;
 import imdb.TitleType;
 
 public class DatasetDeserializer {
 	
 	
-	public static final String outputXmiFilePath = "src/imdb/dataset/output.xmi";
+	public static final String outputXmiFilePath = "src/imdb/dataset/deserialized.xmi";
 	private static Imdb imdb;
 	
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Start deserializing to Ecore instances");
 		DatasetDeserializer.deserialize();
-
-		System.out.println("");
-		System.out.println("Hello Overworld");
+		System.out.println("\nStart serializing to XMI");
 		serializeToXMI(outputXmiFilePath);
-		System.out.println("Hello Overoverworld");
+		System.out.println("Saved to "+outputXmiFilePath);
 		
 	}
 	
@@ -47,28 +45,33 @@ public class DatasetDeserializer {
 		imdb = ImdbFactory.eINSTANCE.createImdb();
 		
 		
-		deserializeSubsetBasics();
+		
+		deserializeTitles();
+		
+		// Hash the titles by id for faster query
+		
+		deserializePersons(); // uses the titles hash
+		
 		
 	}
 	
 	
 	// Create all titles from file
-	public static void deserializeSubsetBasics(){
+	public static void deserializeTitles(){
 		String filename = "src/imdb/dataset/subsetbasics.tsv";
-		System.out.println(System.getProperty("user.dir"));
 		// Open file
 		try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
 		    for(String line; (line = br.readLine()) != null; ) {
 		    	System.out.print(1);
-		    	deserializeSubsetBasicsLine(line);
+		    	deserializeEachTitle(line);
 		    }
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void deserializeSubsetBasicsLine(String line){
-		// Split string by delimitter tab
+	public static void deserializeEachTitle(String line){
+		// Split string by delimiter tab
 		String[] columnValues = line.split("\\t");
 		Title title = ImdbFactory.eINSTANCE.createTitle();
 		title.setImdb(imdb);
@@ -108,9 +111,55 @@ public class DatasetDeserializer {
 		
 	}
 	
+	
+	public static void deserializePersons() {
+		String filename = "src/imdb/dataset/subsetname.tsv";
+		// Open file
+		try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
+		    for(String line; (line = br.readLine()) != null; ) {
+		    	System.out.print(2);
+		    	deserializeEachPerson(line);
+		    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
-	public static void deserializeSubsetName(){
-		String filename = "subsetname.tsv";
+	public static void deserializeEachPerson(String line){
+		// Split string by delimiter tab
+		String[] columnValues = line.split("\\t");
+		Person person = ImdbFactory.eINSTANCE.createPerson();
+		
+		person.setImdb(imdb);
+		
+		String ID = columnValues[0];
+		String name = columnValues[1];
+		
+
+		// If start year is \N, make it a 0
+		int birthYear = 0;
+		try {
+			birthYear = Integer.parseInt(columnValues[2]);
+		} catch (NumberFormatException e) {
+			// do nothing
+		}
+
+		// If start year is \N, make it a 0
+		int deathYear = 0;
+		try {
+			deathYear = Integer.parseInt(columnValues[3]);
+		} catch (NumberFormatException e) {
+			// do nothing
+		}
+		
+
+		String[] professions = columnValues[4].split(",");
+		
+		
+		person.setName(name);
+		person.setBirthYear(birthYear);
+		person.setDeathYear(deathYear);
 		
 	}
 
@@ -134,30 +183,23 @@ public class DatasetDeserializer {
 	
 	public static void serializeToXMI(String filepath) {
 		ResourceSet resourceSet = new ResourceSetImpl();
-		/*
-		* Register XML Factory implementation using DEFAULT_EXTENSION
-		*/
+		// Register XML Factory implementation using DEFAULT_EXTENSION
+		
 		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
 		    "*", new  XMLResourceFactoryImpl());
 		 
-		/*
-		* Create empty resource with the given URI
-		*/
+		// Create empty resource with the given URI
 		Resource resource = resourceSet.createResource(URI.createURI(filepath));
 		 
-		/*
-		* Add instances to contents list of the resource 
-		*/
+		// Add instances to contents list of the resource 
+
 		resource.getContents().add(imdb);
 		 
-		try{
-		    /*
-		    * Save the resource
-		    */
-		      resource.save(null);
-		   }catch (IOException e) {
-		      e.printStackTrace();
-		   }
+		try {
+			resource.save(null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
