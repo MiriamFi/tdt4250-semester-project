b7aecdc9e72536b0032cbde282ee01c4a3953f41
Dabble <ddabble@users.noreply.github.com>
2019-11-30 22:36:44 +0100

    Improved filtering titles by TitleType
    * Can now only select one TitleType
    * Added option to hide other connected objects, i.e. persons
    
    Also removed the auto-generated myService().

--- imdb.odesign/src/imdb/odesign/Services.java
+++ imdb.odesign/src/imdb/odesign/Services.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.ecore.EObject;
@@ -23,14 +24,6 @@ import imdb.TvSeries;
  * The services class used by VSM.
  */
 public class Services {
-
-	/**
-	 * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
-	*/
-	public EObject myService(EObject self, String arg) {
-		// TODO Auto-generated code
-		return self;
-    }
 
 	public Collection<Episode> getSeasons(EObject self) {
 		Map<Integer, Episode> seasons = new HashMap<Integer, Episode>();
@@ -61,7 +54,7 @@ public class Services {
 		return episodes;
 	}
 
-	public Collection<TitleTypeWrapper> getAvailableTitleTypes(Imdb self) {
+	public Set<TitleTypeWrapper> getAvailableTitleTypes(Imdb self) {
 		return self.getTitles().stream()
 				.map(title -> title.getTitleType()).distinct()
 				.map(type -> {
@@ -75,18 +68,41 @@ public class Services {
 	/**
 	 * Use as part of a filter expression.
 	 * 
-	 * @param self
-	 * @param titleType
-	 * @return whether the {@link Title} is of the provided {@link TitleType}.
-	 *         Also returns {@code true} if <b>self</b> is not a {@link Title}.
+	 * @param self each object in a diagram.
+	 * @param titleType a list containing exactly <i>one</i> {@link TitleTypeWrapper}.
+	 * @param hideConnections
+	 * @return If <b>self</b> is a
+	 *         <ul>
+	 *           <li>{@link Title}: whether it's of the provided {@link TitleType}.</li>
+	 *           <li>
+	 *             {@link Person} and <b>hideConnections</b> is {@code true}: whether any of the {@link Title}s
+	 *             they've been involved in are of the provided {@link TitleType}.
+	 *           </li>
+	 *         </ul>
+	 *         Also returns {@code true} if <b>self</b> is neither a {@link Title} nor a {@link Person}.
 	 */
-	public boolean isOneOfTitleTypes(EObject self, Collection<TitleTypeWrapper> titleTypes) {
-		if (!(self instanceof Title))
-			return true;
+	public boolean isFilteredByTitle(EObject self, List<TitleTypeWrapper> titleType, Boolean hideConnections) {
+		if (titleType.size() != 1) {
+			System.err.println("Can't filter by more than one TitleType.");
+			return false;
+		}
+		final TitleType type = titleType.get(0).getTitleType();
 
-		TitleType type = ((Title) self).getTitleType();
-		return titleTypes.stream()
-				.anyMatch(wrapper -> wrapper.getTitleType() == type);
+		if (self instanceof Title) {
+			return ((Title) self).getTitleType() == type;
+		}
+		else if (self instanceof Person) {
+			if (!hideConnections)
+				return true;
+
+			Collection<Involvement> involvements = ((Person) self).getInvolvement();
+			return involvements.stream()
+					.map(involvement -> involvement.getTitle())
+					.anyMatch(title -> title.getTitleType() == type);
+		}
+		else {
+			return true;
+		}
 	}
 
 	/**
