763d2067c2d2922178574570fc726af08ec757ec
Timmy Chan <timmychan301197@gmail.com>
2019-12-01 14:41:31 +0100

    Deserializer for episodes

--- imdb/src/imdb/dataset/DatasetDeserializer.java
+++ imdb/src/imdb/dataset/DatasetDeserializer.java
@@ -3,7 +3,6 @@ package imdb.dataset;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -16,6 +15,7 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
 
+import imdb.Episode;
 import imdb.Imdb;
 import imdb.ImdbFactory;
 import imdb.Involvement;
@@ -23,14 +23,17 @@ import imdb.Person;
 import imdb.Rating;
 import imdb.Title;
 import imdb.TitleType;
+import imdb.TvSeries;
 
 public class DatasetDeserializer {
 	
 	
 	public static final String outputXmiFilePath = "src/imdb/dataset/deserialized.xmi";
 	private static Imdb imdb;
-	
-	private static HashMap<String, Title> titleMap = new HashMap<String, Title>();
+
+
+	private static HashMap<String, Title> allTitleMap = new HashMap<String, Title>();
+	private static HashMap<String, Title> pickedTitleMap = new HashMap<String, Title>();
 	private static HashMap<String, Person> personMap = new HashMap<String, Person>();
 	
 	
@@ -60,9 +63,18 @@ public class DatasetDeserializer {
 		deserializeTitles();
 		System.out.println("Done deserializing all titles");
 		
-		hash500RandomTVSeries();
-		System.out.println("Picked out 500 TV Series titles");
+		pickRandomTitles(TitleType.MOVIE, 5);
+		pickRandomTitles(TitleType.TVSERIES, 50);
 		
+		System.out.println("Picked titles");
+		
+		deserializeEpisodesDetails();
+		System.out.println("Done deserializing episodes by picked titles");
+		
+
+		// Append to ecore model
+		imdb.getTitles().clear();
+		imdb.getTitles().addAll(pickedTitleMap.values());
 		
 		deserializeRatings();
 		System.out.println("Done getting corresponding ratings");
@@ -88,7 +100,7 @@ public class DatasetDeserializer {
 		
 		
 		// For each parsed title, get all persons connected with involvements
-		for (Title title: titleMap.values()) {
+		for (Title title: pickedTitleMap.values()) {
 			for (Involvement involvement: title.getInvolvements()) {
 				smallerPersonMap.put(involvement.getPerson().getID(), involvement.getPerson());
 			}
@@ -97,7 +109,7 @@ public class DatasetDeserializer {
 		// Get each person that is connected to a title in titleMap through knownForTitles
 		imdb.getPersons().forEach(person -> {
 			for (Title title : person.getKnownForTitles()) {
-				if (titleMap.containsValue(title))
+				if (pickedTitleMap.containsValue(title))
 					smallerPersonMap.put(person.getID(), person);
 			}
 		});
@@ -110,6 +122,66 @@ public class DatasetDeserializer {
 		System.out.println("Removed all disconnected persons");
 		
 	}
+	
+	public static void deserializeEpisodesDetails(){
+		String filename = "src/imdb/dataset/episodesDetails.tsv";
+		// Open file
+		try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			// skip the first line, it contains the column headers
+			br.readLine();
+			for(String line; (line = br.readLine()) != null; ) {
+		    	deserializeEpisodeDetails(line);
+		    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void deserializeEpisodeDetails(String line){
+		// Split string by delimiter tab
+		String[] columnValues = line.split("\\t");
+		
+		String episodeID = columnValues[0];
+		String parentTitleID = columnValues[1];
+		
+		// There are episodes that are connected to Movies
+		// We will filter them away
+		Title parentTitle = pickedTitleMap.get(parentTitleID);
+		if (parentTitle == null) return;
+		if (parentTitle.getTitleType() != TitleType.TVSERIES) return;
+		
+				
+		TvSeries series = (TvSeries) parentTitle;
+		Episode episode = (Episode) allTitleMap.get(episodeID);
+		if (episode == null)
+			return;
+		
+		// If start year is \N, make it a 0
+		int seasonNumber = 0;
+		try {
+			seasonNumber = Integer.parseInt(columnValues[1]);
+		} catch (NumberFormatException e) {
+			// do nothing
+		}
+		// If start year is \N, make it a 0
+		int episodeNumber = 0;
+		try {
+			episodeNumber = Integer.parseInt(columnValues[2]);
+		} catch (NumberFormatException e) {
+			// do nothing
+		}
+		episode.setImdb(null);
+		episode.setSeries(series);
+		episode.setEpisodeNumber(episodeNumber);
+		episode.setSeasonNumber(seasonNumber);
+		
+		//pickedTitleMap.put(episodeID, episode);
+	}
+	
+	
+	
+	
+	
 	
 	
 	public static void deserializeTitles(){
@@ -128,15 +200,11 @@ public class DatasetDeserializer {
 	public static void deserializeEachTitle(String line){
 		// Split string by delimiter tab
 		String[] columnValues = line.split("\\t");
-		Title title = ImdbFactory.eINSTANCE.createTitle();
-		title.setImdb(imdb);
-		
-		// Since possibly not all title types are defined, 
 		
 		String titleID = columnValues[0];
-		String titleType = columnValues[1];
+		String titleTypeString = columnValues[1];
 		String titleName = columnValues[2];
-		String titleIsAdult = columnValues[4];
+		//String titleIsAdult = columnValues[4];
 		
 		// If start year is \N, make it a 0
 		int titleStartYear = 0;
@@ -145,7 +213,12 @@ public class DatasetDeserializer {
 		} catch (NumberFormatException e) {
 			// do nothing
 		}
-		String titleEndYear = columnValues[6];
+		int titleEndYear = 0;
+		try {
+			titleEndYear = Integer.parseInt(columnValues[6]);
+		} catch (NumberFormatException e) {
+			// do nothing
+		}
 		
 
 		// If runtimeMinutes is \N, make it a 0
@@ -155,46 +228,61 @@ public class DatasetDeserializer {
 		} catch (NumberFormatException e) {
 			// do nothing
 		}
-		String[] titleGenres = columnValues[8].split(",");
+		// String[] titleGenres = columnValues[8].split(",");
 		
 		
+		TitleType titleType = TitleType.get(titleTypeString.toUpperCase());
+		Title title;
 		
+		if (titleType == TitleType.TVSERIES) {
+			TvSeries tvSeries = ImdbFactory.eINSTANCE.createTvSeries();
+			tvSeries.setEndYear(titleEndYear);
+			title = tvSeries;
+		} else if (titleType == TitleType.TVEPISODE) {
+			Episode episode = ImdbFactory.eINSTANCE.createEpisode();
+			title = episode;
+		} else {
+			title = ImdbFactory.eINSTANCE.createTitle();
+		}
+		title.setImdb(imdb);
 		title.setID(titleID);
-		title.setTitleType(TitleType.get(titleType.toUpperCase()));
+		title.setTitleType(titleType); //null if not
 		
 		title.setName(titleName);
 		title.setStartYear(titleStartYear);
 		title.setRuntime(titleRuntimeMinutes);
-		
+		allTitleMap.put(titleID, title);
 		
 	}
 	
-	private static void hash500RandomTVSeries() {
+	
+	
+	
+	
+	private static List<Title> pickRandomTitles(TitleType titleType, int number) {
 		// Hash the titles by id for faster query
-		// List<T> has the same performance for search as List<T>
-		//imdb.getTitles().forEach(title -> titleMap.put(title.getID(), title));
+		// EList<T> has the same performance for search as List<T>
+		// imdb.getTitles().forEach(title -> titleMap.put(title.getID(), title));
 
 		// Hash titles
-		List<Title> tvSeries = imdb.getTitles().stream()
-					.filter(title -> title.getTitleType() == TitleType.TVSERIES)
+		List<Title> titles = imdb.getTitles().stream()
+					.filter(title -> title.getTitleType() == titleType)
 					.collect(Collectors.toList());
 		
 		// Randomise the list
-		Collections.shuffle(tvSeries);
+		Collections.shuffle(titles);
 		
-		// Pick 500
-		tvSeries = tvSeries.stream()
-				.limit(500) // no need to check if the list is shorter than 500
+		// Pick number of titles
+		titles = titles.stream()
+				.limit(number) // no need to check if the list is shorter than 500
 				.collect(Collectors.toList());
 		
 		// Update the titleMap
-		tvSeries.forEach(series -> titleMap.put(series.getID(), series));
+		titles.forEach(title -> pickedTitleMap.put(title.getID(), title));
 		
-		
-		// Append to ecore model
-		imdb.getTitles().clear();
-		imdb.getTitles().addAll(tvSeries);
+		return titles;
 	}
+	
 	
 	public static void deserializePersons() {
 		String filename = "src/imdb/dataset/persons.tsv";
@@ -241,7 +329,7 @@ public class DatasetDeserializer {
 		String[] knownForTitlesIDs = columnValues[5].split(",");
 		
 		List<Title> knownForTitles = Arrays.asList(knownForTitlesIDs).stream()
-					.map(titleID -> titleMap.get(titleID))	// get Title by id
+					.map(titleID -> pickedTitleMap.get(titleID))	// get Title by id
 					.filter(title -> title != null) 		// remove null titles
 					.collect(Collectors.toList());			// construct the list
 		
@@ -284,7 +372,7 @@ public class DatasetDeserializer {
 		
 		String titleID = columnValues[0];
 		// Stop deserialization when the titleID is not in titleMap
-		if (titleMap.get(titleID) == null) return;
+		if (pickedTitleMap.get(titleID) == null) return;
 		
 		
 		Rating rating = ImdbFactory.eINSTANCE.createRating();
@@ -292,7 +380,7 @@ public class DatasetDeserializer {
 		float averageRating = Float.parseFloat(columnValues[1]);
 		int numVotes = Integer.parseInt(columnValues[2]);
 		
-		rating.setTitle(titleMap.get(titleID));
+		rating.setTitle(pickedTitleMap.get(titleID));
 		rating.setAverageRating(averageRating);
 		rating.setNumberOfVotes(numVotes);
 		
@@ -318,7 +406,7 @@ public class DatasetDeserializer {
 
 		String titleID = columnValues[0];
 		// Stop deserialization when the titleID is not in titleMap
-		if (titleMap.get(titleID) == null) return;
+		if (pickedTitleMap.get(titleID) == null) return;
 		
 		String personID = columnValues[2];
 		// Stop deserialization when the personID is not in personMap
@@ -342,12 +430,14 @@ public class DatasetDeserializer {
 	
 	private static void createInvolvement(String titleID, String personID, String category, String job, String character) {
 		Involvement involvement = ImdbFactory.eINSTANCE.createInvolvement();
-		involvement.setTitle(titleMap.get(titleID)); // both ways
+		involvement.setTitle(pickedTitleMap.get(titleID)); // both ways
 		involvement.setPerson(personMap.get(personID));
 		involvement.setJobCategory(category);
 		involvement.setJob(job);
 		involvement.setCharacter(character);
 	}
+	
+
 	
 	
 	
